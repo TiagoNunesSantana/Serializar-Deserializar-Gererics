61bb3319939d2d416e5be39ee60e592eff32e171
TiagoSantana <tiagonunessantana87@gmail.com>
2020-06-10 18:17:46 -0300

    Add Serializar-Deserializar-Gererics

---
+++ 01_Generics/Serializador.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using System.IO;
+
+namespace _01_Generics
+{
+    public class Serializador
+    {
+        public static void Serializar(object obj)
+        {
+            StreamWriter sw = new StreamWriter(@"C:\Projetos\Topicos Avancados\Aquivos\03_"+obj.GetType().Name+".txt");
+
+            JavaScriptSerializer serializador = new JavaScriptSerializer();
+            string objSerializado = serializador.Serialize(obj);
+
+            sw.Write(objSerializado);
+            sw.Close();
+        } 
+        public static T Deserializar<T>()
+        {
+            StreamReader sr = new StreamReader(@"C:\Projetos\Topicos Avancados\Aquivos\03_"+typeof(T).Name+".txt");
+            string Conteudo = sr.ReadToEnd();
+
+            JavaScriptSerializer serializador = new JavaScriptSerializer();
+            T obj = (T) serializador.Deserialize(Conteudo, typeof(T));
+
+            return obj;
+        }
+    }
+}
