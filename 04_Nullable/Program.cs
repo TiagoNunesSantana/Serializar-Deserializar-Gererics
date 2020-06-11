3846406673baf79fbb1b75c1404bad439b1eca9a
TiagoSantana <tiagonunessantana87@gmail.com>
2020-06-11 15:16:01 -0300

    Nullable: classe que permite colocar um valor null em uma variavel int

---
+++ 04_Nullable/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Nullable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Nullable: classe que permite colocar um valor null em uma variavel int
+            Nullable<int> Idade = null; //ou
+            int? Idade2 = null;
+        }
+    }
+}
